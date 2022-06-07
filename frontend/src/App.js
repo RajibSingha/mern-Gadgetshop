73b5808f82ef9909e727475101d6592f78a9ae51
Rajib Singha <rajib088sny@gmail.com>
2022-06-07 18:55:42 +0530

    Video-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> Gadget Shop </a>
+      </header>
+      <main>LATEST PRODUCT</main>
+    </div>
+  );
+}
+
+export default App;
